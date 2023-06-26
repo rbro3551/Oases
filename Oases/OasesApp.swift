aca5f26d60582aa4b05024c43d1ffda9607ec73b
Riley B <30447344+rbro3551@users.noreply.github.com>
2023-06-26 18:02:13 -0500

    Initial Commit

---
+++ Oases/OasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OasesApp.swift
+//  Oases
+//
+//  Created by Riley Brookins on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct OasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
